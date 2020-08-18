9b5054993c3358427fb2b8d3e4bac31e626e5199
a-a24 <52049029+a-a24@users.noreply.github.com>
2020-08-18 13:30:22 -0400

    skin segmentation

--- seg.m
+++ seg.m
@@ -1,9 +1,21 @@
-CT = dicomread('C:\Users\sethm\Desktop\CT Lymph Nodes\ABD_LYMPH_001\09-14-2014-ABDLYMPH001-abdominallymphnodes-30274\abdominallymphnodes-26828\1-154.dcm');
+CT = dicomread('C:\Users\sethm\Desktop\CT Lymph Nodes\ABD_LYMPH_001\09-14-2014-ABDLYMPH001-abdominallymphnodes-30274\abdominallymphnodes-26828\1-416.dcm');
+
+%Segment the bones
 boneMask = CT > 200;
+
+%Segment the lungs and subtract the bones
 lungMask = CT > 0;
-lungMask1 = lungMask-boneMask;
+lungMask = lungMask-boneMask;
+lungMask = logical(lungMask);
+
+%Segment the background
 background = CT < -800; 
+
+%Segment the skin by subtracting the bones, lungs, and background from the
+%entire image
 skinMask1 = skinMask - boneMask;
+
+%Create dicom files for each mask
 dicomwrite(boneMask, 'bone.dcm')
 dicomwrite(lungMask, 'lung.dcm')
 dicomwrite(skinMask1, 'skin.dcm')
