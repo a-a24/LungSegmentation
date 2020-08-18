35d11c06d9d0a7d0177de6e89aa23524e667dafb
a-a24 <52049029+a-a24@users.noreply.github.com>
2020-08-18 11:25:25 -0400

    skin segmentation

--- seg.m
+++ seg.m
@@ -1,7 +1,8 @@
-CT = dicomread('C:\Users\sethm\Desktop\CT Lymph Nodes\ABD_LYMPH_001\09-14-2014-ABDLYMPH001-abdominallymphnodes-30274\abdominallymphnodes-26828\1-154.dcm');
+CT = dicomread('C:\Users\sethm\Desktop\CT Lymph Nodes\ABD_LYMPH_001\09-14-2014-ABDLYMPH001-abdominallymphnodes-30274\abdominallymphnodes-26828\1-416.dcm');
 boneMask = CT > 200;
-lungMask = CT > -50 && lungMask = CT < 200;
-skinMask = CT < -1000;
+lungMask = CT > -50;
+skinMask = CT > 0;
+skinMask1 = skinMask - boneMask;
 dicomwrite(boneMask, 'bone416.dcm')
 dicomwrite(lungMask, 'lung.dcm')
-dicomwrite(skinMask, 'skin.dcm')
+dicomwrite(skinMask1, 'skin.dcm')
